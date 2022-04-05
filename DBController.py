faae1bef66fb5f34ac4f27ab91c60339f3f77bb5
Sohaib Hussain <sohaib.hussain@uoit.net>
2022-04-05 01:38:10 -0400

    -Added DBController to handle database initialization and functionality. - Added log in functionality on Main Page

---
+++ DBController.py
@@ -0,0 +1,74 @@
+import sqlite3
+from sqlite3 import Error
+
+
+class Database:
+    def __init__(self, db_file):
+        self.__connection = None
+        self.__cursor = None
+        self.__db_file = db_file
+        self.__initializeDatabase()
+        self.__addUserTable()
+        self.addUser("test", "test")
+        self.addUser("test2", "test2")
+
+    def __initializeDatabase(self):
+        self.__connection = None
+        try:
+            self.__connection = sqlite3.connect(self.__db_file)
+            self.__cursor = self.__connection.cursor()
+            print(sqlite3.version)
+        except Error as e:
+            print(e)
+            self.__initializeDatabase()
+
+    def __addUserTable(self):
+        query = "CREATE TABLE Users (id INTEGER,username TEXT NOT NULL UNIQUE,password TEXT NOT NULL " \
+                "UNIQUE,PRIMARY KEY(id AUTOINCREMENT)); "
+
+        if self.__cursor is not None:
+            try:
+                self.__cursor.execute(query)
+                print("Table Created Successfully successfully")
+            except Error as e:
+                print e
+        self.__connection.commit()
+
+    def checkUserExists(self, username):
+        queryCheck = "SELECT * FROM Users WHERE username = ?"
+        exists = False
+        if self.__cursor is not None:
+            try:
+                self.__cursor.execute(queryCheck, [username])
+                rows = self.__cursor.fetchall()
+                if rows is not None and len(rows) > 0:
+                    exists = True
+                return exists
+            except Error as e:
+                print e
+                return exists
+
+    def validateUser(self, username, password):
+        queryCheck = "SELECT * FROM Users WHERE username = ? and password = ?"
+        valid = False
+        if self.__cursor is not None:
+            try:
+                self.__cursor.execute(queryCheck, [username, password])
+                rows = self.__cursor.fetchall()
+                if rows is not None and len(rows) > 0:
+                    valid = True
+            except Error as e:
+                print e
+            finally:
+                return valid
+
+    def addUser(self, username, password):
+        queryInsert = "INSERT INTO Users(username,password) VALUES (?,?)"
+        if self.__cursor is not None:
+            try:
+                if not self.checkUserExists(username):
+                    self.__cursor.execute(queryInsert, (username, password))
+                    print("Added successfully")
+            except Error as e:
+                print e
+        self.__connection.commit()
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import database as db
+import DBController
 
 if __name__ == "__main__":
-    db.initializeSoftware("test.db")
+    dbController = DBController.Database("test12.db")
+    userLoggedIn = False
+
+    while not userLoggedIn:
+        username = raw_input("Username: ")
+        password = raw_input("Password: ")
+        valid = dbController.validateUser(username, password)
+        if valid:
+            userLoggedIn = True
+        else:
+            print "Invalid Credentials"
+
+    print "LoggedIn"
